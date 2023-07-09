cefb4eb51fe50ef8b02b5f8efc5e8c0a574eac74
Kodake <kodake@hotmail.es>
2023-07-08 20:30:47 -0400

    Change app name

--- android/app/src/main/java/com/frontend/MainActivity.java
+++ android/app/src/main/java/com/frontend/MainActivity.java
@@ -1,4 +1,4 @@
-package com.customers;
+package com.customers_app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "customers";
+    return "customers_app";
   }
 
   /**
--- ios/frontendTests/frontendTests.m
+++ ios/frontendTests/frontendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface customersTests : XCTestCase
+@interface customers_appTests : XCTestCase
 
 @end
 
-@implementation customersTests
+@implementation customers_appTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
